7dc2e5a815cbd63cfb5300020476a13895168bd3
spacetimed <spacetimedecor@gmail.com>
2020-12-06 21:17:20 +0000

    sockets

--- src/services/clientSocket.js
+++ src/services/clientSocket.js
@@ -1,6 +1,9 @@
 import {io} from 'socket.io-client';
 const PORT = process.env.PORT || 3231
-const socket = io(`https://seldon-dashboard.herokuapp.com:${PORT}`, {
+const production  = 'https://seldon-dashboard.herokuapp.com/';
+const development = `http://localhost:${PORT}/`;
+const URL = (process.env.NODE_ENV ? production : development);
+const socket = io(`${URL}`, {
     transports: ['websocket', 'polling']
 });
 
