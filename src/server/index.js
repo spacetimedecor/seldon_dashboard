4b4f649ac4b2f29dc6d3d8176ee56b3c9119c4de
spacetimed <spacetimedecor@gmail.com>
2020-12-12 03:38:45 +0000

    Redux websockets

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
-const io = (module.exports.io = require("socket.io")(server));
+const io = require("socket.io")(server);
+
 const machineManager = require("./MachineManager.js");
 
-const PORT = process.env.REACT_APP_PORT || 3231;
+const PORT = process.env.PORT || 3231;
 
 app.use(express.static(__dirname + "/../../build"));
 
