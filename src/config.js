9d84741e9d6bcf76abffc0b8d20f155abc652388
spacetimed <spacetimedecor@gmail.com>
2020-12-13 16:41:32 +0000

    Server settings

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const PORT = process.env.PORT || 3231
 export const production  = 'https://seldon-dashboard.herokuapp.com/';
 export const development = `http://localhost:${PORT}/`;
-export const URL = (process.env.NODE_ENV ? development : production);
+export const URL = (process.env.NODE_ENV === 'development' ? development : production);
